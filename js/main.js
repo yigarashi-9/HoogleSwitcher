01939d7eb349fa2b9181ef463fb4d5e79c3ae8c6
yu-i9 <yuu.igarashi.9@gmail.com>
2016-01-22 10:12:06 +0900

    Add contents

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var searchButton = document.getElementById('search');
+
+  searchButton.addEventListener('click', (function() {
+    var query = document.getElementById('form').query.value;
+    var engine = document.querySelector('input[name="engine"]:checked').value;
+    var urls = {
+      'hackage': 'https://www.haskell.org/hoogle/?hoogle=',
+      'lts': 'https://www.stackage.org/lts/hoogle?q=',
+      'nightly': 'https://www.stackage.org/nightly/hoogle?q='
+    };
+    var url = urls[engine] + query;
+    chrome.tabs.create({url: url});
+  }));
+});
