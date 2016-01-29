b008497d9f7aea948ca46f3b7dfe749677f1bc50
yu-i9 <yuu.igarashi.9@gmail.com>
2016-01-29 10:16:53 +0900

    Add an event page for initialization

---
+++ src/js/background.js
@@ -0,0 +1,12 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.local.get('snapshots', function(storage){
+    if(!storage.snapshots){
+      // initialize local storage
+      chrome.storage.local.set({'snapshots': {
+        'hackage': {'name': 'Hackage', 'prim': true},
+        'lts': {'name': 'LTS (latest)', 'prim': true},
+        'nightly': {'name': 'Nightly (latest)', 'prim': true}
+      }});
+    }
+  });
+})
