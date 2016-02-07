0e38d7ff76a3fc2aafc3f3d2eb9edcbbb9ab180d
yu-i9 <yuu.igarashi.9@gmail.com>
2016-02-07 13:37:19 +0900

    Update DOM directly in message.js

--- src/js/lib/message.js
+++ src/js/lib/message.js
@@ -1,23 +1,12 @@
 module.exports.send = (function(){
 
   function sendMessage(label, body){
-    var message = {'body': body, 'label': label};
-    chrome.storage.local.set({'message': message});
+    var p = document.getElementById('message');
+    p.className = '';
+    p.classList.add('message', label);
+    p.textContent = body;
   }
 
   return {'success': sendMessage.bind(null, 'message-success'),
           'error': sendMessage.bind(null, 'message-error')};
 }());
-
-module.exports.get = new Promise(
-  function(resolve){
-    chrome.storage.local.get('message', function(storage){
-      if(!chrome.runtime.lastError && storage.message){
-        resolve(storage.message);
-      }
-    });
-  });
-
-module.exports.clear = function(){
-  chrome.storage.local.remove('message');
-};
