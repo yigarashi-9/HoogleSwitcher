119448c5eca2d4454f981a342903f6f207c684ce
yu-i9 <yuu.igarashi.9@gmail.com>
2016-03-15 21:09:53 +0900

    Introduced gulp-watchify

--- gulpfile.js
+++ gulpfile.js
@@ -3,36 +3,39 @@ var es = require('event-stream');
 var eslint = require('gulp-eslint');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
+var watchify = require('gulp-watchify');
 var zip = require('gulp-zip');
 
-var js_path = './src/js/';
-var js_files = [
-  'popup.js',
-  'options.js',
-  'background.js'
-]
+var path = {
+  'src': './src/js/*.js',
+  'dest': './apps/js/',
+  'appSrc': ['./apps/**/*', 'manifest.json'],
+  'appZip': 'apps.zip',
+  'appDest': './'
+}
 
-gulp.task('build', function(){
-  var tasks = js_files.map(function(entry){
-    return browserify({entries: [js_path + entry]})
-      .bundle()
-      .pipe(source(entry))
-      .pipe(gulp.dest('./apps/js'));
-  });
-  return es.merge.apply(null, tasks);
-})
+var watching = false;
+gulp.task('enable-watch', function(){ watching = true; });
+
+gulp.task('browserify', watchify(function(watchify){
+  return gulp.src(path.src)
+    .pipe(watchify({watch: watching}))
+    .pipe(gulp.dest(path.dest));
+}));
+
+gulp.task('watchify', ['enable-watch', 'browserify']);
 
 gulp.task('test', function(){
-  return gulp.src('./src/js/*.js')
+  return gulp.src(path.src)
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
 gulp.task('zip', function(){
-  return gulp.src(['./apps/**/*', 'manifest.json'])
-    .pipe(zip('apps.zip'))
-    .pipe(gulp.dest('./'))
+  return gulp.src(path.appSrc)
+    .pipe(zip(path.appZip))
+    .pipe(gulp.dest(path.appDest))
 });
 
-gulp.task('pack', ['build', 'zip']);
+gulp.task('pack', ['browserify', 'zip']);
