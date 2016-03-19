f4270aaf464560ef7d9a975411c80fa4d069ff98
yu-i9 <yuu.igarashi.9@gmail.com>
2016-03-19 13:42:46 +0900

    tslint

--- src/ts/background.ts
+++ src/ts/background.ts
@@ -2,7 +2,7 @@ import snapshot = require("./Models/Snapshot");
 
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.local.get("snaplist", function(storage) {
-    if(!storage["snaplist"]){
+    if (!storage["snaplist"]) {
       chrome.storage.local.set({"snaplist": snapshot.SnapshotList.initialData()});
     }
   });
